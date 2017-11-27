5ab7ea16c496148c228cd05aafd7952c98475f27
Elyahu Borodin <elyahu.de@gmail.com>
2017-11-27 22:46:30 +0100

    language

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>OnlineShiurim</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -41,6 +41,16 @@
                 top: 18px;
             }
 
+            .bsad {
+              font-size: 12px;
+              font-weight: 600;
+              letter-spacing: .1rem;
+              text-decoration: none;
+                position: absolute;
+                right: 18px;
+                top: 18px;
+            }
+
             .content {
                 text-align: center;
             }
@@ -66,6 +76,9 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
+          <div class="bsad">
+            בס"ד
+          </div>
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -79,15 +92,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    OnlineShiurim
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://goo.gl/U4pTR1">Deutsch</a>
+                    <a href="https://goo.gl/G8mC94">Русский</a>
+                    <a href="https://goo.gl/Rv3AJE">English</a>
                 </div>
             </div>
         </div>
