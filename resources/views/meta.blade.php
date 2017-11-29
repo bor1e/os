c5bd2c35436a947e86584e0b73264e1192206e1f
Elyahu Borodin <elyahu.de@gmail.com>
2017-11-29 22:39:48 +0100

    favicon source update

--- resources/views/meta.blade.php
+++ resources/views/meta.blade.php
@@ -3,5 +3,5 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="icon" href="http://127.0.0.1:8000/images/favicon_B.png" type="image/png" alt="onlineshiurim favicon" />
+<link rel="icon" href="/images/favicon.png" type="image/png" alt="onlineshiurim favicon" />
 <title>OnlineShiurim</title>
