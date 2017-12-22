3801adeb38650273643d0325348b27d4ae9a074a
Elyahu Borodin <elyahu.de@gmail.com>
2017-12-22 01:08:23 +0100

    courses auth:user added help line

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -27,6 +27,8 @@
           <p class="mt-3">Please <a href="{{route('login') }}">sign in</a> to enroll and to participate in classes.</p>
         @elseif (Auth::user()->hasRole('pending'))
           Please <a href="/profile"> update </a>your User Information, in order to be approved by the Admins.
+        @else
+          <p class="lead">Please be passioned, while we are reviewing you apllication. If the course you interested in, is about to start, feel free to <a href="/about-us#contact">contact</a> us.</p>
         @endif
         </li>
       </ul>
