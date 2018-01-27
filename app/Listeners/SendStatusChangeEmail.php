cd134f3d043c6e02228f6d0fd2db5c3957e6c738
bor1e <elyahu.de@gmail.com>
2018-01-27 21:31:42 +0100

    enrollment prepares

--- app/Listeners/SendStatusChangeEmail.php
+++ app/Listeners/SendStatusChangeEmail.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class SendStatusChangeEmail
+class SendStatusChangeEmail implements ShouldQueue
 {
     /**
      * Create the event listener.
