74c15d0f602e54aff052b13fb10319babc918fd0
bor1e <elyahu.de@gmail.com>
2018-01-27 21:43:17 +0100

    everything uptodate

---
+++ app/Jobs/EnrollmentJob.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use App\User;
+use App\Mail\EnrollmentMail;
+use Illuminate\Support\Facades\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class EnrollmentJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $user;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if(!$this->user->participates()->count())
+            return;
+
+        Mail::to($this->user->email)
+            ->send(new EnrollmentMail($this->user->participates()));
+    }
+}
